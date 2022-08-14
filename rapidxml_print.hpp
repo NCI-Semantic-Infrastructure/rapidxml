d4a7f6a99e0994f85aa108f6d90ab1fc55b6ef39
NCIEVS <Gilberto.Fragoso@nih.gov>
2022-08-14 13:31:54 -0400

    gcc 4.7+

--- rapidxml_print.hpp
+++ rapidxml_print.hpp
@@ -101,7 +101,45 @@ namespace rapidxml
 
         ///////////////////////////////////////////////////////////////////////////
         // Internal printing operations
-    
+ 		/*
+		These must be included in rapidxml_print (at line 104) to make it work:
+		https://stackoverflow.com/questions/14113923/rapidxml-print-header-has-undefined-methods
+
+		Easier to move print_node to the bottom of this section . . .
+
+		updated git with my copy
+		*/ 
+
+		template<class OutIt, class Ch>
+		inline OutIt print_children(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_attributes(OutIt out, const xml_node<Ch> *node, int flags);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_data_node(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_cdata_node(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_element_node(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_declaration_node(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_comment_node(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_doctype_node(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		template<class OutIt, class Ch>
+		inline OutIt print_pi_node(OutIt out, const xml_node<Ch> *node, int flags, int indent);
+  
+		/*	On g++ 4.7+.  Not sure on VC tbh 
+		*/
+   
         // Print node
         template<class OutIt, class Ch>
         inline OutIt print_node(OutIt out, const xml_node<Ch> *node, int flags, int indent)
